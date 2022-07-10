9fab1d09b573ffaa5031f440f06893023b55806f
georgelemnosanu <georgelemnosanu@gmail.com>
2022-07-10 20:26:20 +0300

    Initial commit

---
+++ functionchoose.js
@@ -0,0 +1,11 @@
+
+let redBtn=document.getElementById("redbook");
+let yellowBtn=document.getElementById("yellowbook");
+let greenBtn=document.getElementById("greenbook");
+let books =document.getElementById("book");
+
+function change(){
+
+books.backgroundImage = "url(https://img5.goodfon.com/wallpaper/nbig/4/70/vyveska-neon-slovo-knigi-svet-krasnyi.jpg)"
+
+}
